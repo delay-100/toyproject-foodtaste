754f96a14dab3644619f60f6a67574f694837b66
delay100 <qor3127@naver.com>
2022-03-30 17:13:16 +0900

    add comment

--- router/auth.js
+++ router/auth.js
@@ -2,7 +2,7 @@ const express = require('express'); // node web framework
 
 const router = express.Router();
 
-
+// GET /join - 회원가입 페이지 라우터
 router.get('/join', (req, res) => {
     res.render('join', { title: '회원 가입'});
 });
