ab5adfbac0a563a49477d9000fce97d17f4da637
delay100 <qor3127@naver.com>
2022-04-01 14:25:08 +0900

    add models setting

--- models/index.js
+++ models/index.js
@@ -4,6 +4,11 @@ const config = require('../config/config')[env];
 
 const User = require('./user');
 
+const Koreanfood = require('./koreanfood');
+const Chinesefood = require('./chinesefood');
+const Japanesefood = require('./japanesefood');
+const Westernfood = require('./westernfood');
+
 const db = {};
 
 const sequelize = new Sequelize(
@@ -12,9 +17,17 @@ const sequelize = new Sequelize(
 
 db.sequelize = sequelize;
 db.User = User;
+db.Koreanfood = Koreanfood;
+db.Chinesefood = Chinesefood;
+db.Japanesefood = Japanesefood;
+db.Westernfood = Westernfood;
 
 // 각 객체 실행
 User.init(sequelize);
+Koreanfood.init(sequelize);
+Chinesefood.init(sequelize);
+Japanesefood.init(sequelize);
+Westernfood.init(sequelize);
 
 // 관계 연결
 // User.associate(db);
