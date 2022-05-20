b8c1ea17822c2d4ce265b16b7ca5e77e992f623c
delay100 <qor3127@naver.com>
2022-05-21 01:21:23 +0900

    backend gogo,,?

--- routes/analyze.js
+++ routes/analyze.js
@@ -4,4 +4,8 @@ const { Food, Select, User } = require('../models');
 
 const router = express.Router();
 
+router.get('/', isLoggedIn, async (req, res, next) => {
+    res.render('analyze' , );
+});
+
 module.exports = router;
