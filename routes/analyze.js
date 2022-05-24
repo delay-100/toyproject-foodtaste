86088fa46bfdfa3061b0bc3cb43f37ccc998a4c8
delay100 <qor3127@naver.com>
2022-05-24 17:08:45 +0900

    no error~~good

--- routes/analyze.js
+++ routes/analyze.js
@@ -4,36 +4,36 @@ const { Food, Select, User } = require('../models');
 
 const router = express.Router();
 
-console.log('11');
+// console.log('11');
 
 router.get('/', isLoggedIn, async (req, res, next) => {
     res.render('analyze' );
 });
 
-router.post('/analyzeSelected', isLoggedIn, async (req, res, next) => {
-    try{
-        const { result }= req.body;
-        if(!result)return res.json({status:'false'});
+// router.post('/analyzeSelected', isLoggedIn, async (req, res, next) => {
+//     try{
+//         const { result }= req.body;
+//         if(!result)return res.json({status:'false'});
         
 
-        let map = {};
-        console.log("-------------");
-        console.log(result);
-        console.log("-------------");
-        // 로그인한 사용자
-        // for(let i=0; i<result.length)
-        // map.push(i:); 
+//         let map = {};
+//         console.log("-------------");
+//         console.log(result);
+//         console.log("-------------");
+//         // 로그인한 사용자
+//         // for(let i=0; i<result.length)
+//         // map.push(i:); 
         
 
 
-        const selects = await Select.findAll({
-            raw: true,
-          });
+//         const selects = await Select.findAll({
+//             raw: true,
+//           });
 
-        return res.json({status:'true'});
-    } catch(err){
-        console.error(err);
-    }
-});
+//         return res.json({status:'true'});
+//     } catch(err){
+//         console.error(err);
+//     }
+// });
 
 module.exports = router;
